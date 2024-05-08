cf6aa32da7c4e0950a9f9edeb7cfe0bf37d4d9dd
mdah <courseteam@Courses-MacBook-Pro.local>
2024-05-08 15:27:45 +0800

    fix: kinde route try 2

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,13 +1,10 @@
-// vercel compile error, adjusted lines below
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
 
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextRequest, NextResponse } from 'next/server'; //add NextResponse
-
-export default async function handler( //add default, swap GET with handler
-    request: NextRequest, 
+export async function GET(
+    request: NextRequest,
     { params }: any
-): Promise<Response> { //add proimse<repsonse>
-  const endpoint = params.kindeAuth;
-  const authHandler = handleAuth(request, endpoint); //all new
-  return await authHandler(request, NextResponse);
+): Promise<any> { //add Promise<any> to resolve deployment compile error
+        const endpoint = params.kindeAuth
+        return await handleAuth(request, endpoint)
 }
