a272e9e7abd713c251da353864c4a2d4dca66acc
mdah <courseteam@Courses-MacBook-Pro.local>
2024-05-08 16:15:48 +0800

    fix: auth-callback add suspense boundary

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,46 +1,50 @@
-"use client"
+'use client';
 
-import { trpc } from "@/app/_trpc/client"
-import { Loader2 } from "lucide-react"
-import { useRouter, useSearchParams } from "next/navigation"
-import { useEffect } from "react"
+import { trpc } from '@/app/_trpc/client';
+import { Loader2 } from 'lucide-react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { Suspense, useEffect } from 'react';
 
 const Page = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    const searchParams = useSearchParams()
-    const origin = searchParams.get('origin')
+  const searchParams = useSearchParams();
+  const origin = searchParams.get('origin');
 
-    // mod since onSuccess not in useQuery,@brokenquotes_en and @amanxsharma20 comments in video
-    const {data, error} = trpc.authCallback.useQuery(undefined);
-    
-    useEffect(() => {
-        if (data?.success) {
-            // user is synced to db
-            router.push(origin ? `/${origin}` : `/dashboard`);
-        } else if (error?.data?.code==="UNAUTHORIZED") {console.log('error: ', error);
-    router.push("/sign-in");
-}
-    }, [data, error, router, origin]);
+  // mod since onSuccess not in useQuery,@brokenquotes_en and @amanxsharma20 comments in video
+  const { data, error } = trpc.authCallback.useQuery(undefined);
 
-    // how to handle error if it is thrown, is this missing?
-    /*
+  useEffect(() => {
+    if (data?.success) {
+      // user is synced to db
+      router.push(origin ? `/${origin}` : `/dashboard`);
+    } else if (error?.data?.code === 'UNAUTHORIZED') {
+      console.log('error: ', error);
+      router.push('/sign-in');
+    }
+  }, [data, error, router, origin]);
+
+  // how to handle error if it is thrown, is this missing?
+  /*
         retry: true,
         retryDelay: 500,
     */
 
-        return (
-            <div className='w-full mt-24 flex justify-center'>
-                <div className="flex flex-col items-center gap-2">
-                    <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
-                    <h3 className="font-semibold text-xl">Setting up your account...</h3>
-                    <p>You will be redirected automatically.</p>
-                </div>
-            </div>
-        )
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+        <h3 className='font-semibold text-xl'>Setting up your account...</h3>
+        <p>You will be redirected automatically.</p>
+      </div>
+    </div>
+  );
+};
 
+const SuspendedPage = () => ( //
+    <Suspense fallback={<div>Loading...</div>}>
+        <Page />
+    </Suspense>
+)
 
-
-    }
-
-export default Page
+export default SuspendedPage;
