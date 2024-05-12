c7377a0a779cf47f66b1400957d81dcfea3f9f11
mdah <courseteam@Courses-MacBook-Pro.local>
2024-05-12 11:03:39 +0800

    update: stripe webhook secret

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,19 +1,7 @@
 import BillingForm from "@/components/BillingForm"
 import { getUserSubscriptionPlan } from "@/lib/stripe"
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const Page = async () => {
-    const router = useRouter();
-
-    useEffect(() => {
-      // Check if the current URL contains the unwanted part
-      if (router.asPath.includes('-jr3ifqlxh-mdahs-projects')) {
-        // Redirect the user to the desired URL
-        router.replace('/dashboard/billing');
-      }
-    }, [router]);
-
  const subscriptionPlan = await getUserSubscriptionPlan()
 
  return <BillingForm subscriptionPlan={subscriptionPlan}/>
