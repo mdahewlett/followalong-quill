5ccbb206e26e628d3807b1e18f219ff005aaa515
mdah <courseteam@Courses-MBP.hitronhub.home>
2024-05-13 09:29:57 +0800

    chore: update marketing copy with speedy reader

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = async () => {
       <MaxWidthWrapper>
         <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
           <Link href='/' className='flex z-40 font-semibold'>
-            <span>Follow Along.</span>
+            <span>Speedy Reader</span>
           </Link>
 
           <MobileNav isAuth={!!user} subscriptionPlan={subscriptionPlan}/> {/*pass subscription plan*/}
